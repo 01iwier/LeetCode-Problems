db2365aed3bc4437ba36c5772cbb7034bfbb8971
Oliwier <wiecko.oli@gmail.com>
2024-10-09 19:26:44 +0100

    new

---
+++ 22.generate-parentheses.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=22 lang=java
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+import java.util.*;
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<String>();
+        recurse(result, 0, 0, "", n);
+        return result;
+    }
+
+    public void recurse(List<String> result, int left, int right, String s, int n) {
+        if (s.length() == n * 2) {
+            result.add(s);
+            return;
+        }
+
+        if (left < n) {
+            recurse(result, left + 1, right, s + "(", n);
+        }
+
+        if (right < left) {
+            recurse(result, left, right + 1, s + ")", n);
+        }
+    }
+}
+// @lc code=end
+
